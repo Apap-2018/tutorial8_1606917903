787552c079b38de5f00f4f0967a90ce523012e55
fathyann <nasya05@yahoo.com>
2018-11-07 15:10:18 +0700

    latihan 1

---
+++ src/main/java/com/apap/tutorial8/controller/UserRoleController.java
@@ -0,0 +1,34 @@
+package com.apap.tutorial8.controller;
+
+import java.util.regex.Pattern;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.apap.tutorial8.model.UserRoleModel;
+import com.apap.tutorial8.service.UserRoleService;
+
+@Controller
+@RequestMapping("/user")
+public class UserRoleController {
+	@Autowired
+	private UserRoleService userService;
+	
+	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
+	private String addUserSubmit(@ModelAttribute UserRoleModel user, Model model) {
+		userService.addUser(user);
+		model.addAttribute("msg", "User berhasil ditambah");
+		return "home";
+	}
+}
